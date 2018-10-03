d89b87452c2fdddf1eb0d293617208fcfdc17940
elf <thecatsareruling@gmail.com>
2018-10-03 12:44:12 -0400

    Update project structure, add maven project

---
+++ test/edu/bsu/cs222/finalproject/TestMain.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222.finalproject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMain {
+    @Test
+    public void testMain() {
+        Assert.assertTrue(true);
+    }
+}
