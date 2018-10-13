11ff5e80d2d1f91af0b8c6fb4b4cee5dc5b5f54f
danielbatterystapler <danielbatterystapler@gmail.com>
2018-10-12 22:58:36 -0400

    add basic capabilities to AddPurchase

--- src/edu/bsu/cs222/finalproject/database/WorkingLayer.java
+++ src/edu/bsu/cs222/finalproject/database/WorkingLayer.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs222.finalproject.database;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 public class WorkingLayer {
     private Database database;
 
@@ -20,5 +23,16 @@ public class WorkingLayer {
 
     public void addNewUser(User newUser){
         database.insertUser(newUser);
+    }
+
+    public Purchase addNewPurchase(User existingPurchaser, Item newItem){
+        database.insertItem(newItem);
+        Purchase purchase = new Purchase();
+        purchase.itemId = newItem.id;
+        purchase.purchaserId = existingPurchaser.id;
+        purchase.date = new Date(Calendar.getInstance().getTime().getTime());//make it the current time for now, this will be added to the ui later
+        purchase.notes = "";//right now there are no notes, this will be added to ui later
+        database.insertPurchase(purchase);
+        return purchase;
     }
 }
--- src/edu/bsu/cs222/finalproject/ui/AddPurchase.java
+++ src/edu/bsu/cs222/finalproject/ui/AddPurchase.java
@@ -1,9 +1,13 @@
 package edu.bsu.cs222.finalproject.ui;
 
+import edu.bsu.cs222.finalproject.database.Item;
+import edu.bsu.cs222.finalproject.database.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.File;
 
@@ -20,10 +24,27 @@ public class AddPurchase {
         main.stage.show();
     }
 
-    @FXML UserSelector userSelector = null;
-
     @FXML
     void backToSelection() throws Exception{
         MainSelection.showScene();
+    }
+
+    @FXML UserSelector userField = null;
+    @FXML TextField modelField = null;
+    @FXML TextField serialField = null;
+    @FXML Label errorLabel = null;
+
+    @FXML
+    void submit() throws Exception{
+        User user = userField.getUser();
+        if(user == null){
+            errorLabel.setText("You must Select a User First");
+        }
+        Item item = new Item();
+        item.modelNumber = modelField.getText();
+        item.serialNumber = serialField.getText();
+        Main main = Main.getInstance();
+        main.workingLayer.addNewPurchase(user, item);
+        errorLabel.setText("Purchase Successfully Added!");
     }
 }
