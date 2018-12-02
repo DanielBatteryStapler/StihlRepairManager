76c8c1f8a18d3cfa0cc08263ba1d534dd32c1cde
danielbatterystapler <danielbatterystapler@gmail.com>
2018-12-02 15:09:49 -0500

    add CSS and HTML for Repair Printout

--- src/edu/bsu/cs222/finalproject/backend/Print.java
+++ src/edu/bsu/cs222/finalproject/backend/Print.java
@@ -41,7 +41,7 @@ public class Print {
 
         StringBuilder output = new StringBuilder();
 
-        //adding Strings together is usually a bad idea because Java uses copy-on-write with Strings, but the compiler will be able to figure out the string literals
+        //adding Strings together is usually a bad idea because Java uses copy-on-write with Strings, but the compiler will be able to figure out the string literals and make it efficient
         output.append(""
                 + "<!DOCTYPE html>"
                 + "<html>"
@@ -68,30 +68,173 @@ public class Print {
             URL logoB = main.getClass().getResource("/print/logoB.png");
             output.append(""
                     + "<div id='header'>"
+                    + "<div id='headerTop'>"
                     + "<div id='headerImages'>"
-                    + "<img src='" + logoA + "' />"
-                    + "<img src='" + logoB + "' />"
-                    + "</div>"
+                    + "<img src='"); output.append(logoA); output.append("' />"
+                    + "<img src='"); output.append(logoB); output.append("' />"
+                    + "</div>"//headerImages
                     + "<div id='headerText'>"
                     + "105 S. Forest Ave.<br />"
                     + "Brazil, IN<br />"
                     + "47834<br />"
                     + "812-448-2877<br />"
-                    + "</div>"
-                    + "</div>"
+                    + "</div>"//header text
+                    + "</div>"//headerTop
+                    + "<div id='headerBottom'>"
+                    + "<div id ='dateHeader'>"
+                    + "Date"
+                    + "</div>"//dateHeader
+                    + "<div id='dateBox'>");
+                    output.append(repair.dateStarted.toLocaleString());
+                    output.append(""
+                    + "</div>"//dateBox
+                    + "<div id='contactCustomerHeader'>"
+                    + "MUST CONTACT CUSTOMER WITH ESTIMATE?"
+                    + "</div>"//contactCustomerHeader
+                    + "<div id='contactCustomerBox'>"
+                    + "TEST"
+                    + "</div>"//contactCustomerBox
+                    + "</div>"//headerBottom
+                    + "</div>"//header
             );
         }
-        {//customerInfo
-
-        }
-        {//description
-
-        }
+        output.append(""
+            + "<table id='posTableRoot' class='posTable'>"
+            + "<tr class='posTable'>"
+            + "<td rowspan='2' class='posTable'>"
+        );
         {//itemList
+            output.append(""
+                    + "<div id='itemList'>"
+                    + "<table>"
+                    + "<tr>"
+                    + "<th style='width:50px;'>QTY</th>"
+                    + "<th style='width:250px;'>Name</th>"
+                    + "<th style='width:50px;'>Price</th>"
+                    + "<th style='width:50px;'>Amount</th>"
+                    + "</tr>"
+            );
+            for(int i = 0; i < 19; i++){//just fill the table with a bunch of empty elements for now
+                output.append(""
+                    + "<tr>"
+                    + "<td></td>"
+                    + "<td></td>"
+                    + "<td></td>"
+                    + "<td></td>"
+                    + "</tr>"
+                );
+            }
+            //add the totals on the bottom
+            output.append(""
+                + "<tr>"
+                + "<th></th>"
+                + "<th class='align-right'>Total Materials</th>"
+                + "<td></td>"
+                + "<td></td>"
+                + "</tr>"
 
+                + "<tr>"
+                + "<th></th>"
+                + "<th class='align-right'>Total Labor</th>"
+                + "<td></td>"
+                + "<td></td>"
+                + "</tr>"
+
+                + "<tr>"
+                + "<th></th>"
+                + "<th class='align-right'>Tax</th>"
+                + "<td></td>"
+                + "<td></td>"
+                + "</tr>"
+
+                + "<tr>"
+                + "<th></th>"
+                + "<th class='align-right'>Grand Total</th>"
+                + "<td></td>"
+                + "<td></td>"
+                + "</tr>"
+            );
+
+            output.append(""
+                    + "</table>"
+                    + "</div>"//itemList
+            );
         }
-        {//footer
+        output.append(""
+                + "</td>"
+                + "<td class='posTable'>"
+        );
+        {//customerInfo
+            output.append(""
+                    + "<div id='customerInfo'>"
+                    + "<b>&nbsp;&nbsp;&nbsp;Customer Information</b>"
+                    + "<table>"
 
+                    + "<tr>"
+                    + "<th>Name</th>"
+                    + "<td style='width:210px;'></td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<th>Address</th>"
+                    + "<td></td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<th>City</th>"
+                    + "<td></td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<th>State</th>"
+                    + "<td></td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<th>Phone</th>"
+                    + "<td></td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<th>Model #</th>"
+                    + "<td></td>"
+                    + "</tr>"
+
+                    + "</table>"
+                    + "</div>"//customerInfo
+            );
+        }
+        output.append(""
+                + "</td>"
+                + "</tr>"
+                + "<tr class='posTable'>"
+                + "<td class='posTable'>"
+        );
+        {//description
+            output.append(""
+                    + "<div id='description'>"
+                    + "Description of Issue or Repairs Needed:"
+                    + "<div id='descriptionTextBox'>"
+                    + "</div>"//descriptionTextBox
+                    + "</div>"//description
+            );
+        }
+        output.append(""
+                + "</td>"
+                + "</tr>"
+                + "</table>"//posTable
+        );
+        {//footer
+            output.append(""
+                    + "Repairs Completed:"
+                    + "<div id='repairsCompletedTextBox'>"
+                    + "</div>"//repairsCompletedTextBox
+                    + "<div id='legalBox'>"
+                    + "By signing below, I agree to allow Brazil ACE Hardware to complete the work as described in the notes section of this work "
+                    + "order. I also agree to pay for all Parts and Labor charges, not covered under warranty, associated with any repairs "
+                    + "completed. I agree to pay a minimum $15.00 diagnostic fee if I choose not to have any repairs completed."
+                    + "</div>"//legalBox
+            );
         }
         output.append(""
                 + "</body>"
